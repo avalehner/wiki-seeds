f989d706081213e7f85a9f88b9cbdb0a51465cb5
Ava Lehner <avalehner@gmail.com>
2025-10-05 13:28:20 -0400

    detailed view styling update

--- app/components/DetailedView.tsx
+++ app/components/DetailedView.tsx
@@ -3,6 +3,7 @@ import { SavedArticle, SummaryResponse } from "../src/interfaces";
 import { getArticleSummary } from "../src/dataFetching";
 import ToWikipediaPageButton from "./ToWikipediaPageButton";
 import styles from "./../styles/DetailedView.module.css"; 
+import { getWikipediaUrlFromTitle } from "../src/util";
 
 interface DetailedViewProps {
   savedArticle: SavedArticle;
@@ -23,28 +24,33 @@ export default function DetailedView(props: DetailedViewProps) {
   return (
     <div className={styles.detailedViewContainer}>
       <div className={styles.savedArticleTitleContainer}>{props.savedArticle.article.title}</div>
-      <div className={styles.locationContainer}>
-        <div className={styles.detailedViewLabel}>Location</div>
-        <div className={styles.locationCoords}>
-         {props.savedArticle.article.lat},{" "}
-        {props.savedArticle.article.lon} 
+      <div className={styles.articleDetailsContainer}>
+        <div className={styles.locationContainer}>
+          <div className={styles.detailedViewLabel}>Location</div>
+          <div className={styles.locationCoords}>
+          {props.savedArticle.article.lat},{" "}
+          {props.savedArticle.article.lon} 
+          </div>
         </div>
+        <div className={styles.dateCollectedContainer}>
+          <div className={styles.detailedViewLabel}>Date collected</div>
+          <div className={styles.dateCollected}>{props.savedArticle.timestampFound.toLocaleDateString()}</div>
+        </div>
+        <div className={styles.snippetAndOpenWikiContainer}>
+          <div className={styles.detailedViewLabel}>Snippet</div>
+          <div>
+            <a
+            href={getWikipediaUrlFromTitle(props.savedArticle.article.title)}
+            target="_blank"
+            rel="noreferrer"
+            >
+              <div className={styles.openOnWikiBtn}>open on wikipedia</div>
+            </a>
+          </div>
+        </div>
+        <div className={styles.summaryContainer}>{summary && summary.extract}</div>
       </div>
-      <div className={styles.dateCollectedContainer}>
-        <div className={styles.detailedViewLabel}>Date collected</div>
-        <div className={styles.dateCollected}>{props.savedArticle.timestampFound.toLocaleDateString()}</div>
-      </div>
-      {/* <div className={styles.detailedViewLabel}>Rarity</div> */}
-      <div className={styles.snippetAndOpenWikiContainer}>
-        <div className={styles.detailedViewLabel}>Snippet</div>
-        <div>
-        <ToWikipediaPageButton
-          articleTitle={props.savedArticle.article.title}
-        />
-      </div>
-      </div>
-      <div className={styles.snippetContainer}>{summary && summary.extract}</div>
-      <button onClick={props.goToFlowerDex}>(x)</button>
+      <div className={styles.goToFlowerDexBtnContainer}><button onClick={props.goToFlowerDex}>(close)</button></div>
     </div>
   );
 }
