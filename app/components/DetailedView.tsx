2b89d7699361269f854cba8cc490da26a88f0758
Ava Lehner <avalehner@gmail.com>
2025-10-05 14:03:42 -0400

    detailed view style update

--- app/components/DetailedView.tsx
+++ app/components/DetailedView.tsx
@@ -4,6 +4,9 @@ import { getArticleSummary } from "../src/dataFetching";
 import ToWikipediaPageButton from "./ToWikipediaPageButton";
 import styles from "./../styles/DetailedView.module.css"; 
 import { getWikipediaUrlFromTitle } from "../src/util";
+import { getFlowerImageFromArticle } from "../src/util";
+import Image from "next/image";
+
 
 interface DetailedViewProps {
   savedArticle: SavedArticle;
@@ -23,6 +26,15 @@ export default function DetailedView(props: DetailedViewProps) {
 
   return (
     <div className={styles.detailedViewContainer}>
+      <div className={styles.articleImageContainer}>
+         <Image
+          src={getFlowerImageFromArticle(props.savedArticle.article)}
+          alt="A flower"
+          width="100"
+          height="100"
+          className={styles.articleImage}
+        />
+      </div>
       <div className={styles.savedArticleTitleContainer}>{props.savedArticle.article.title}</div>
       <div className={styles.articleDetailsContainer}>
         <div className={styles.locationContainer}>
@@ -36,21 +48,35 @@ export default function DetailedView(props: DetailedViewProps) {
           <div className={styles.detailedViewLabel}>Date collected</div>
           <div className={styles.dateCollected}>{props.savedArticle.timestampFound.toLocaleDateString()}</div>
         </div>
-        <div className={styles.snippetAndOpenWikiContainer}>
-          <div className={styles.detailedViewLabel}>Snippet</div>
-          <div>
-            <a
-            href={getWikipediaUrlFromTitle(props.savedArticle.article.title)}
-            target="_blank"
-            rel="noreferrer"
-            >
-              <div className={styles.openOnWikiBtn}>open on wikipedia</div>
-            </a>
+        <div className={styles.snippetContainer}>
+          <div className={styles.snippetAndOpenWikiContainer}>
+            <div className={styles.detailedViewLabel}>Snippet</div>
+            {/* open wikipedia btn */}
+            <div>
+              <a
+              href={getWikipediaUrlFromTitle(props.savedArticle.article.title)}
+              target="_blank"
+              rel="noreferrer"
+              >
+                <div className={styles.openOnWikiBtn}>open on wikipedia</div>
+              </a>
+            </div>
           </div>
+          <div className={styles.summaryContainer}>{summary && summary.extract}</div>
         </div>
-        <div className={styles.summaryContainer}>{summary && summary.extract}</div>
+        <div className={styles.closeBtnContainer}>
+          <button onClick={props.goToFlowerDex}>
+            <Image
+            src="/images/close-button.svg"
+            alt="Close"
+            width="25"
+            height="25"
+            className={styles.closeButtonImage}
+            />
+          <div>close</div>
+          </button>
+        </div>
       </div>
-      <div className={styles.goToFlowerDexBtnContainer}><button onClick={props.goToFlowerDex}>(close)</button></div>
     </div>
   );
 }
