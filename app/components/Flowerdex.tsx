deaf6b3e7c7d64a5c23e38817b44ecb285daf974
marcos-acosta <macosta1625@gmail.com>
2025-10-05 13:58:26 -0400

    Set up flowerdex

--- app/components/Flowerdex.tsx
+++ app/components/Flowerdex.tsx
@@ -1,5 +1,12 @@
 import { Article, Page, SavedArticle } from "../src/interfaces";
 import PageToggle from "./PageToggle";
+import styles from "./../styles/FlowerDex.module.css";
+import {
+  articleToThemeColor,
+  getFlowerImageFromArticle,
+  hexToRgb,
+} from "../src/util";
+import Image from "next/image";
 
 interface FlowerdexProps {
   savedArticles: SavedArticle[];
@@ -9,19 +16,45 @@ interface FlowerdexProps {
 
 export default function Flowerdex(props: FlowerdexProps) {
   return (
-    <div>
-      {props.savedArticles.map((savedArticle) => (
-        <div key={savedArticle.article.pageid}>
-          <button onClick={() => props.goToArticle(savedArticle.article)}>
-            <div>{savedArticle.article.title}</div>
-          </button>
-        </div>
-      ))}
-      <PageToggle
-        currentPage={Page.FLOWER_DEX}
-        goToMap={props.goToMap}
-        goToFlowerDex={() => {}}
-      />
+    <div className={styles.flowerDexOuterContainer}>
+      <div className={styles.flowerDexGrid}>
+        {props.savedArticles.map((savedArticle) => {
+          const backgroundColor = articleToThemeColor(
+            savedArticle.article
+          ).backgroundColor;
+          const backgroundColorRgb = hexToRgb(backgroundColor);
+          const rgbString = `${backgroundColorRgb?.r}, ${backgroundColorRgb?.g}, ${backgroundColorRgb?.b}`;
+          const backgroundStyle = `rgb(${rgbString})`;
+          const flowerImage = getFlowerImageFromArticle(savedArticle.article);
+          return (
+            <div
+              key={savedArticle.article.pageid}
+              className={styles.flowerOuterContainer}
+            >
+              <button
+                onClick={() => props.goToArticle(savedArticle.article)}
+                className={styles.flowerInnerContainer}
+                style={{ backgroundColor: backgroundStyle }}
+              >
+                <Image
+                  src={flowerImage}
+                  alt="A flower"
+                  width="200"
+                  height="200"
+                  className={styles.flowerImage}
+                />
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.toggleContainer}>
+        <PageToggle
+          currentPage={Page.FLOWER_DEX}
+          goToMap={props.goToMap}
+          goToFlowerDex={() => {}}
+        />
+      </div>
     </div>
   );
 }
